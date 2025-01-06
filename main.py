c6b57de549688775582b4044496a6fe68f958abf
asukh <sukhavasiabilash@gmail.com>
2025-01-05 23:46:01 -0500

    added ruler lines to pdf book

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import fpdf
 import pandas
 
 pdf = fpdf.FPDF(orientation="P",format='A4',unit='mm')
-
-
+pdf.set_auto_page_break(auto=False,margin=0)
 
 df = pandas.read_csv("topics.csv")
 print(df["Topic"])
@@ -12,12 +11,28 @@ print(df["Pages"])
 for index, row in df.iterrows():
     for i in range(int(row["Pages"])):
         pdf.add_page()
+        pdf.set_fill_color(100, 100, 100)
+        #Header
         pdf.set_font(family="Times", style="B", size=24)
         pdf.set_text_color(50,100,50)
-        pdf.cell(w=0,h=12,txt=row["Topic"],align="L",ln=1,border = 0)
+        pdf.cell(w=0,h=12,txt=row["Topic"],align="L",border = 0)
         pdf.set_line_width(2)
         pdf.line(10,20,200,20)
 
+        # ruled lines
+        for line in range(30,270,10):
+            pdf.set_line_width(0.25)
+            pdf.line(10,line,200,line)
+
+        # Footer
+        pdf.ln(260)
+        pdf.set_line_width(1)
+
+
+        pdf.line(10, 270, 200, 270)
+        pdf.set_font(family="Times",style="I",size=10)
+        pdf.set_text_color(254,0,0)
+        pdf.cell(w=0,h=10,txt=row["Topic"],align="R",border=0)
 
 # pdf.cell(w=0,h=12,txt="Hello I am pdf line1",align="L",ln=1,border=0)
 # pdf.cell(w=0,h=12,txt="HI I am pdf line2",align="L",ln=1,border=0)
--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-temp = 3
+# temp = 3
+#
+# for i in range(temp):
+#     print(i)
 
-for i in range(temp):
-    print(i)
+
+for line in range(20,50,10):
+    print(line)
